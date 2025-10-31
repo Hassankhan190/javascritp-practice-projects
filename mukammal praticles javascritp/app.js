ac4babf72858e48f0871bb374bb02f1ab31fa073
Hassankhan190 <hassankhan142007@email.com>
2025-10-31 11:14:53 +0500

    second commit

---
+++ mukammal praticles javascritp/app.js
@@ -0,0 +1,68 @@
+// let num1 = prompt("Enter your number first");
+// let num2 = prompt("Enter your number second");
+// let avg =(num1 + num2 / 2);
+// if(avg > 50){
+//     console.log("Pass")
+// }else{
+//     console.log("Fail")
+// }
+
+// let age = Number(prompt("Enter your age"));
+// let isCitizen = true;
+
+// if(age >= 18 && isCitizen){
+//    console.log("Eligible for Vote");
+// }else{
+//    console.log("Not Eligible");
+// }
+
+// age += 1;   // assignment operator
+// console.log("Next year your age will be:", age);
+// let marks = 40;
+
+// if(marks >= 50){
+//   console.log("You passed the exam");
+// }else{
+//   console.log("You failed the exam");
+// }
+
+// let temp = 35;
+
+// if(temp > 40){
+//   console.log("It's too hot!");
+// }else if(temp >= 25){
+//   console.log("Weather is warm");
+// }else if(temp >= 15){
+//   console.log("It's cool outside");
+// }else{
+//   console.log("It's cold");
+// }
+
+// let num = prompt("Enter your number");
+// let grate;
+// if(num >= 90 && num <= 100){
+//     grate = "A+"
+// }else if(num >= 80 && num <= 89){
+//      grate = "B"
+// }else if(num >= 70 && num <= 79){
+//       grate = "C"
+// }else if(num >= 60 && num <= 69){
+//      grate = "D"
+// }else{
+//    grate = "fail"
+// }
+    
+// console.log("you grate is",grate);
+
+
+let color = prompt("Enter your color (red, yellow, green)").toLowerCase();
+
+if(color === "red"){
+    console.log("Stop");
+}else if(color === "yellow"){
+    console.log("Ready to go");
+}else if(color === "green"){
+    console.log("Go");
+}else{
+    console.log("Invalid color");
+}
